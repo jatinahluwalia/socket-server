4e382fc8d55af835c0011efe593a0e0648e751ef
jitu2404 <103820854+jitu2404@users.noreply.github.com>
2023-09-28 22:53:20 +0530

    server

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import express from "express";
 import { createServer } from "http";
 import cors from "cors";
 import { Server } from "socket.io";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
@@ -12,9 +12,6 @@ const app = express();
 app.use(cors());
 
 const server = createServer(app);
-server.listen(PORT, () => {
-  console.log(`Listening server at ${PORT}`);
-});
 
 app.get("/", (_req, res) => {
   return res.send("Working");
@@ -41,4 +38,8 @@ io.on("connection", (socket) => {
   });
 });
 
-module.exports = server;
+server.listen(PORT, () => {
+  console.log(`Listening server at ${PORT}`);
+});
+
+export default server;
